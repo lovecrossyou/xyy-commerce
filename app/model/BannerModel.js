8e675826590e04ec7f084521903cd7627a35de28
lovecross <lovecrossyoutt@hotmail.com>
2019-03-26 23:58:08 +0800

    xx

---
+++ app/model/BannerModel.js
@@ -0,0 +1,50 @@
+const { ROLE_CUSTOMER } = require('../common/role');
+
+module.exports = app => {
+  const { INTEGER, STRING, DATE, UUID, UUIDV4 } = app.Sequelize;
+
+  const BannerModel = app.model.define('banner', {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      // autoIncrement: true,
+    },
+    image_path: {
+      type: STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    title: {
+      type: STRING(20),
+      allowNull: false,
+    },
+    type: {
+      type: STRING(10),
+      allowNull: false,
+      defaultValue: ROLE_CUSTOMER,
+    },
+    extra: {
+      type: STRING(50),
+      allowNull: true,
+    },
+    createTime: {
+      type: DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+    updateTime: {
+      type: DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+  });
+
+  BannerModel.beforeBulkUpdate(user => {
+    user.attributes.updateTime = new Date();
+    return user;
+  });
+
+  return BannerModel;
+};
